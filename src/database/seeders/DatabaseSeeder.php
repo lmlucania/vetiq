08e528ef6eacaac93b3ef450c9da60f44e10bf31
M.maeda <petit.chat.0509@gmail.com>
2024-12-28 23:15:01 +0900

    feat:DatabaseSeederを更新

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -17,6 +17,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             HospitalSeeder::class,
             StaffSeeder::class,
+            MenuSeeder::class,
+            VetSeeder::class,
         ]);
     }
 }
