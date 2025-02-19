92e3b6c6fd2da0d71e89d99836b029a2bf96f1a4
M.maeda <petit.chat.0509@gmail.com>
2025-02-19 23:41:51 +0900

    feat:migrationファイルを作成

---
+++ src/database/migrations/2025_02_19_232708_create_business_hours.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('business_hours', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('hospital_id')->constrained()->onDelete('cascade')->comment('病院ID');
+            $table->tinyInteger('day_of_week')->comment('曜日');
+            $table->time('start_time')->comment('受付開始時間');
+            $table->time('end_time')->comment('受付終了時間');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('business_hours');
+    }
+};
