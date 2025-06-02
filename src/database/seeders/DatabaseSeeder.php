44e4d80bcb60446bc69a23ef5fa2db6df49cb407
lmlucania <petit.chat.0509@gmail.com>
2025-06-02 15:36:33 +0900

    feat:seeder作成

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -19,6 +19,7 @@ class DatabaseSeeder extends Seeder
             StaffSeeder::class,
             MenuSeeder::class,
             VetSeeder::class,
+            UserSeeder::class,
         ]);
     }
 }
---
+++ src/database/seeders/UserSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::factory()->count(10)->create();
+
+        User::factory()->create([
+            'email' => 'user+1@example.com',
+        ]);
+    }
+}
