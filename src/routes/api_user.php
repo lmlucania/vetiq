16c6e983060c328e27fab9156bb7660510cf8ebf
lmlucania <petit.chat.0509@gmail.com>
2025-06-02 15:44:00 +0900

    fix:use文を修正

--- src/routes/api_user.php
+++ src/routes/api_user.php
@@ -2,13 +2,13 @@
 
 declare(strict_types=1);
 
-use App\Http\Controllers\Hospital\AuthController;
+use App\Http\Controllers\User\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['guest'])->group(static function () {
     Route::post('login', [AuthController::class, 'login'])->name('login');
 });
 
-Route::middleware('auth:staffs')->group(static function () {
+Route::middleware('auth:users')->group(static function () {
     Route::post('logout', [AuthController::class, 'logout'])->name('logout');
 });
