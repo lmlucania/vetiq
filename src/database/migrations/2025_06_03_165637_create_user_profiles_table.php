f7fd8eda01273adab78ce4e8a68a6067552ee6fe
lmlucania <petit.chat.0509@gmail.com>
2025-06-03 17:25:55 +0900

    feat:migrationファイルを作成

---
+++ src/database/migrations/2025_06_03_165637_create_user_profiles_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_profiles', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade')->unique();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('first_name_kana')->nullable();
+            $table->string('last_name_kana')->nullable();
+            $table->string('phone_number');
+            $table->string('post_code')->nullable();
+            $table->integer('prefecture')->nullable();
+            $table->string('address1')->nullable();
+            $table->string('address2')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_profiles');
+    }
+};
