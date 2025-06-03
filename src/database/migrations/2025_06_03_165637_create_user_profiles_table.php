b6b5e58f19fafef09704e04e0f4cd46be561dd03
lmlucania <petit.chat.0509@gmail.com>
2025-06-04 00:05:01 +0900

    fix:ユニークキーの貼り方を修正

--- src/database/migrations/2025_06_03_165637_create_user_profiles_table.php
+++ src/database/migrations/2025_06_03_165637_create_user_profiles_table.php
@@ -13,7 +13,8 @@ return new class extends Migration
     {
         Schema::create('user_profiles', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained('users')->onDelete('cascade')->unique();
+            $table->unsignedBigInteger('user_id')->unique();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->string('first_name');
             $table->string('last_name');
             $table->string('first_name_kana');
