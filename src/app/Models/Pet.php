a01a1c7e126fc02fe9397062dbf67be0c91528ad
lmlucania <petit.chat.0509@gmail.com>
2025-06-03 16:23:30 +0900

    feat:ソフトデリートを追加

--- src/app/Models/Pet.php
+++ src/app/Models/Pet.php
@@ -7,10 +7,11 @@ namespace App\Models;
 use App\Domains\Pet\Enum\Gender;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Pet extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $fillable = [
         'uuid',
