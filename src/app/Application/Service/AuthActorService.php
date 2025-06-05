6a66f0fea8ce17b7ad91db976baf12b04c772fc9
lmlucania <petit.chat.0509@gmail.com>
2025-06-05 09:03:45 +0900

    feat:スタッフのidや病院idを返す処理を追加

--- src/app/Application/Service/AuthActorService.php
+++ src/app/Application/Service/AuthActorService.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Application\Service;
 
+use App\Domains\Hospital\ValueObjects\HospitalId;
 use App\Exceptions\UnauthorizedException;
 use App\Models\StaffModel;
 use App\Models\User;
@@ -28,12 +29,38 @@ class AuthActorService
         return $this->authUser instanceof User;
     }
 
-    public function getUserId(): int
+    public function getStaff(): StaffModel
+    {
+        if (! ($this->isStaff())) {
+            throw new UnauthorizedException('スタッフ以外は許可されていません。');
+        }
+
+        return $this->authUser;
+    }
+
+    public function getUser(): User
     {
         if (! ($this->isUser())) {
             throw new UnauthorizedException('ユーザー以外は許可されていません。');
         }
 
-        return $this->authUser->id;
+        return $this->authUser;
+    }
+
+    public function getStaffId(): int
+    {
+        return $this->getStaff()->id;
+    }
+
+    public function getUserId(): int
+    {
+        return $this->getUser()->id;
+    }
+
+    // fixme 一旦value objectを返すようにする
+    public function getHospitalId(): HospitalId
+    {
+        $id = $this->getStaff()->hospital_id;
+        return new HospitalId($id);
     }
 }
