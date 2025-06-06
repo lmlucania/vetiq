d7ff5e403f5ec6f7db51059b7f8fe9d858665371
lmlucania <petit.chat.0509@gmail.com>
2025-06-06 22:44:09 +0900

    feat:migrationファイルを作成

---
+++ src/database/migrations/2025_06_06_215949_create_reviews_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('hospital_id')->constrained()->onDelete('cascade');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->tinyInteger('rating')->unsigned()->comment('評価点数');
+            $table->string('title')->comment('タイトル');
+            $table->text('body')->nullable()->comment('本文');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reviews');
+    }
+};
