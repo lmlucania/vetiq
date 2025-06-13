24d7c9031e24fdc64760ecb9444be4bd78ef79ca
lmlucania <petit.chat.0509@gmail.com>
2025-06-14 08:59:41 +0900

    test:例外予約受付時間の登録、削除

--- src/tests/Feature/Controllers/Hospital/ExceptionHourControllerTest.php
+++ src/tests/Feature/Controllers/Hospital/ExceptionHourControllerTest.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Feature\Controllers\Hospital;
 
 use App\Domains\Schedule\Enum\DayOfWeek;
@@ -28,45 +30,44 @@ class ExceptionHourControllerTest extends TestCase
     }
 
     /**
-     * 予約受付時間の一覧 情報が取得できること
+     * 例外予約受付時間の一覧 情報が取得できること
      */
     public function testIndexSuccess()
     {
         // 準備（Arrange）
+        $this->actingAs($this->staff, $this->guard);
         ExceptionHour::factory()->create([
             'hospital_id' => $this->hospital->id,
-            'date' => '2025-01-01',
+            'date'        => '2025-01-01',
             'time_period' => TimePeriod::AM,
             'start_time'  => '09:00',
             'end_time'    => '12:00',
-            'is_closed' => false,
-            'reason' => 'テスト1',
+            'is_closed'   => false,
+            'reason'      => 'テスト1',
         ]);
         ExceptionHour::factory()->create([
             'hospital_id' => $this->hospital->id,
-            'date' => '2025-01-01',
+            'date'        => '2025-01-01',
             'time_period' => TimePeriod::PM,
             'start_time'  => '13:00',
             'end_time'    => '16:00',
-            'is_closed' => false,
-            'reason' => 'テスト2',
+            'is_closed'   => false,
+            'reason'      => 'テスト2',
         ]);
         ExceptionHour::factory()->create([
             'hospital_id' => $this->hospital->id,
-            'date' => '2025-01-02',
+            'date'        => '2025-01-02',
             'time_period' => TimePeriod::AM,
             'start_time'  => '10:00',
             'end_time'    => '13:00',
-            'is_closed' => false,
-            'reason' => 'テスト3',
+            'is_closed'   => false,
+            'reason'      => 'テスト3',
         ]);
 
         // ログインしない病院のデータをセットアップ
         ExceptionHour::factory()->create([
             'hospital_id' => Hospital::factory()->create(),
         ]);
-
-        $this->actingAs($this->staff, $this->guard);
 
         // 実行（Act）
         $response = $this->get(route('hospital.exception-hours.index', ['year' => 2025]));
@@ -77,34 +78,224 @@ class ExceptionHourControllerTest extends TestCase
             ->assertJsonCount(3, 'data')
             ->assertJsonFragment(
                 [
-                    'date' => '2025-01-01',
+                    'date'        => '2025-01-01',
                     'time_period' => TimePeriod::AM,
                     'start_time'  => '09:00',
                     'end_time'    => '12:00',
-                    'is_closed' => false,
-                    'reason' => 'テスト1',
+                    'is_closed'   => false,
+                    'reason'      => 'テスト1',
                 ],
             )
             ->assertJsonFragment(
                 [
-                    'date' => '2025-01-01',
+                    'date'        => '2025-01-01',
                     'time_period' => TimePeriod::PM,
                     'start_time'  => '13:00',
                     'end_time'    => '16:00',
-                    'is_closed' => false,
-                    'reason' => 'テスト2',
+                    'is_closed'   => false,
+                    'reason'      => 'テスト2',
                 ],
             )
             ->assertJsonFragment(
                 [
-                    'date' => '2025-01-02',
+                    'date'        => '2025-01-02',
                     'time_period' => TimePeriod::AM,
                     'start_time'  => '10:00',
                     'end_time'    => '13:00',
-                    'is_closed' => false,
-                    'reason' => 'テスト3',
+                    'is_closed'   => false,
+                    'reason'      => 'テスト3',
                 ],
             );
     }
 
+    /**
+     * 例外予約受付時間の一覧 0件でもエラーにならないこと
+     */
+    public function testIndexNoRecordSuccess()
+    {
+        // 準備（Arrange）
+        $this->actingAs($this->staff, $this->guard);
+
+        // 実行（Act）
+        $response = $this->get(route('hospital.exception-hours.index'));
+
+        // 検証（Assert）
+        $response
+            ->assertStatus(200)
+            ->assertJsonCount(0, 'data');
+    }
+
+    /**
+     * 指定した曜日の受付時間の作成/更新 作成されること
+     */
+    public function testStoreSuccess()
+    {
+        // 準備（Arrange）
+        $postData = [
+            'day_of_week' => DayOfWeek::FRIDAY->value,
+            'periods'     => [
+                [
+                    'time_period' => TimePeriod::AM->value,
+                    'start_time'  => '09:00',
+                    'end_time'    => '12:30',
+                ],
+                [
+                    'time_period' => TimePeriod::PM->value,
+                    'start_time'  => '13:00',
+                    'end_time'    => '19:30',
+                ],
+            ],
+        ];
+        $this->actingAs($this->staff, $this->guard);
+
+        // 実行（Act）
+        $response = $this->post(route('hospital.business_hours.store'), $postData);
+
+        // 検証（Assert）
+        $response->assertStatus(200);
+
+        $this->assertDatabaseCount('business_hours', 2);
+    }
+
+    /**
+     * 指定した曜日の受付時間の作成/更新 作成と更新されること
+     */
+    public function testStoreCreateRecordAndUpdateRecordSuccess()
+    {
+        // 準備（Arrange）
+        $this->actingAs($this->staff, $this->guard);
+        ExceptionHour::factory()->create([
+            'hospital_id' => $this->hospital->id,
+            'date'        => '2025-01-01',
+            'time_period' => TimePeriod::AM,
+            'start_time'  => '10:00',
+            'end_time'    => '13:00',
+            'is_closed'   => false,
+            'reason'      => 'テスト1',
+        ]);
+        $postData = [
+            'date'    => '2025-01-01',
+            'periods' => [
+                [
+                    'time_period' => TimePeriod::AM->value,
+                    'start_time'  => '09:00',
+                    'end_time'    => '12:30',
+                    'is_closed'   => false,
+                    'reason'      => 'テスト2',
+                ],
+                [
+                    'time_period' => TimePeriod::PM->value,
+                    'start_time'  => '13:00',
+                    'end_time'    => '19:30',
+                    'is_closed'   => false,
+                    'reason'      => 'テスト3',
+                ],
+            ],
+        ];
+
+        // 実行（Act）
+        $response = $this->post(route('hospital.exception-hours.store'), $postData);
+
+        // 検証（Assert）
+        $response->assertStatus(200);
+
+        $this->assertDatabaseCount('exception_hours', 2);
+
+        $records = ExceptionHour::orderBy('id', 'asc')->get();
+        $this->assertEquals('2025-01-01', $records[0]->date->format('Y-m-d'));
+        $this->assertEquals(TimePeriod::AM, $records[0]->time_period);
+        $this->assertEquals('09:00', $records[0]->start_time->format('H:i'));
+        $this->assertEquals('12:30', $records[0]->end_time->format('H:i'));
+        $this->assertFalse($records[0]->is_closed);
+        $this->assertEquals('テスト2', $records[0]->reason);
+
+        $this->assertEquals('2025-01-01', $records[1]->date->format('Y-m-d'));
+        $this->assertEquals(TimePeriod::PM, $records[1]->time_period);
+        $this->assertEquals('13:00', $records[1]->start_time->format('H:i'));
+        $this->assertEquals('19:30', $records[1]->end_time->format('H:i'));
+        $this->assertFalse($records[1]->is_closed);
+        $this->assertEquals('テスト3', $records[1]->reason);
+    }
+
+    /**
+     * 指定した日付の例外受付時間の作成/更新 作成と更新されること hospital_id、date、time_periodで重複して登録されないこと
+     */
+    public function testStoreNotDuplicateSuccess()
+    {
+        // 準備（Arrange）
+        $this->actingAs($this->staff, $this->guard);
+        $postData = [
+            'date'    => '2025-01-01',
+            'periods' => [
+                [
+                    'time_period' => TimePeriod::AM->value,
+                    'start_time'  => null,
+                    'end_time'    => null,
+                    'is_closed'   => true,
+                    'reason'      => 'テスト2',
+                ],
+                [
+                    'time_period' => TimePeriod::AM->value,
+                    'start_time'  => '13:00',
+                    'end_time'    => '19:30',
+                    'is_closed'   => false,
+                    'reason'      => 'テスト3',
+                ],
+            ],
+        ];
+
+        // 実行（Act）
+        $response = $this->post(route('hospital.exception-hours.store'), $postData);
+
+        // 検証（Assert）
+        $response->assertStatus(200);
+
+        $this->assertDatabaseCount('exception_hours', 1);
+
+        $record = ExceptionHour::first();
+        $this->assertEquals('2025-01-01', $record->date->format('Y-m-d'));
+        $this->assertEquals(TimePeriod::AM, $record->time_period);
+        $this->assertEquals('13:00', $record->start_time->format('H:i'));
+        $this->assertEquals('19:30', $record->end_time->format('H:i'));
+        $this->assertFalse($record->is_closed);
+        $this->assertEquals('テスト3', $record->reason);
+    }
+
+    /**
+     * 例外予約受付時間の削除 削除できていること
+     */
+    public function testDestroySuccess()
+    {
+        // 準備（Arrange）
+        $model = ExceptionHour::factory()->create([
+            'hospital_id' => $this->hospital->id,
+        ]);
+        $this->actingAs($this->staff, $this->guard);
+
+        // 実行（Act）
+        $response = $this->delete(route('hospital.exception-hours.destroy', ['exception_hour' => $model->id]));
+
+        // 検証（Assert）
+        $response->assertStatus(200);
+
+        $this->assertDatabaseCount('exception_hours', 0);
+    }
+
+    /**
+     * 例外予約受付時間の削除 他の病院の予約受付時間を削除できないこと
+     */
+    public function testDestroyNotHospitalOwnFailure()
+    {
+        // 準備（Arrange）
+        $model = ExceptionHour::factory()->create([
+            'hospital_id' => Hospital::factory()->create(),
+        ]);
+        $this->actingAs($this->staff, $this->guard);
+
+        // 実行（Act）
+        $response = $this->delete(route('hospital.exception-hours.destroy', ['exception_hour' => $model->id]));
+
+        // 検証（Assert）
+        $response->assertStatus(404);
+    }
 }
