ea32b8786ac62c9fcbf322e5cdcd419b24837abf
lmlucania <petit.chat.0509@gmail.com>
2025-06-15 16:54:27 +0900

    feat:お知らせのリポジトリを作成

---
+++ src/app/Infrastructure/Repositories/NotificationRepository.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Infrastructure\Repositories;
+
+use App\Domains\Notification\Repository\NotificationRepositoryInterface;
+use App\Models\Menu;
+use App\Models\Notification;
+use Carbon\Carbon;
+
+class NotificationRepository implements NotificationRepositoryInterface
+{
+    public function getByHospitalIdAndId(int $hospitalId, int $id): Notification
+    {
+        return Notification::where('hospital_id', $hospitalId)->findOrFail($id);
+    }
+
+    public function create(string $uuid, int $hospitalId, string $title, string $detail, bool $isPublished, ?Carbon $publishedAt): Notification
+    {
+        return Notification::create([
+            'uuid'         => $uuid,
+            'hospital_id'  => $hospitalId,
+            'title'        => $title,
+            'detail'       => $detail,
+            'is_published' => $isPublished,
+            'published_at' => $publishedAt,
+        ]);
+    }
+
+    public function update(int $id, string $title, string $detail, bool $isPublished, ?Carbon $publishedAt): bool
+    {
+        $notification = Notification::findOrFail($id);
+
+        $notification->title        = $title;
+        $notification->detail       = $detail;
+        $notification->is_published = $isPublished;
+        $notification->published_at = $publishedAt;
+
+        return $notification->save();
+    }
+
+    public function delete(int $id): bool
+    {
+        return Notification::findOrFail($id)->delete();
+    }
+}
