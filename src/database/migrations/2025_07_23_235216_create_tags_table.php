a29f6840776c5e6bb798fff4cca403847239505f
lmlucania <petit.chat.0509@gmail.com>
2025-07-27 00:13:38 +0900

    feat:migrationファイルを作成

---
+++ src/database/migrations/2025_07_23_235216_create_tags_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tag_categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->unique();
+            $table->timestamps();
+        });
+
+        Schema::create('tags', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('tag_category_id')->constrained('tag_categories')->onDelete('cascade')->comment('タグカテゴリーID');
+            $table->string('name')->unique();
+            $table->timestamps();
+        });
+
+        Schema::create('hospital_tag', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('hospital_id')->constrained('hospitals')->onDelete('cascade')->comment('病院ID');
+            $table->foreignId('tag_id')->constrained('tags')->onDelete('cascade')->comment('タグID');
+            $table->timestamps();
+
+            $table->unique(['hospital_id', 'tag_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tags');
+        Schema::dropIfExists('hospital_tag');
+    }
+};
