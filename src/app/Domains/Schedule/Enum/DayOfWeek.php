a64f6c95ee55e25d8bb907096cda7436553e1e48
lmlucania <petit.chat.0509@gmail.com>
2025-08-23 23:18:20 +0900

    fix:enumの値を変更

--- src/app/Domains/Schedule/Enum/DayOfWeek.php
+++ src/app/Domains/Schedule/Enum/DayOfWeek.php
@@ -6,24 +6,24 @@ namespace App\Domains\Schedule\Enum;
 
 enum DayOfWeek: int
 {
-    case SUNDAY    = 1;
-    case MONDAY    = 2;
-    case TUESDAY   = 3;
-    case WEDNESDAY = 4;
-    case THURSDAY  = 5;
-    case FRIDAY    = 6;
-    case SATURDAY  = 7;
+    case MONDAY    = 1;
+    case TUESDAY   = 2;
+    case WEDNESDAY = 3;
+    case THURSDAY  = 4;
+    case FRIDAY    = 5;
+    case SATURDAY  = 6;
+    case SUNDAY    = 7;
 
     public function label(): string
     {
         return match ($this) {
-            self::SUNDAY    => '日',
             self::MONDAY    => '月',
             self::TUESDAY   => '火',
             self::WEDNESDAY => '水',
             self::THURSDAY  => '木',
             self::FRIDAY    => '金',
             self::SATURDAY  => '土',
+            self::SUNDAY    => '日',
         };
     }
 }
